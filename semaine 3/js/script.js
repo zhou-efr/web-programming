a9ecba6b3c9418ffe861a6f0290ab5cb50ebb630
zhou <killian.zhou@sepefrei.fr>
2021-04-16 18:00:35 +0200

    semaine 3 - do a barrel roll

--- semaine 3/js/script.js
+++ semaine 3/js/script.js
@@ -1,11 +1,18 @@
 console.log("marie-pierre");
-let counter = 0;
-let anti_counter = 10;
-const timeout = 10;
+//document.getElementById("html").style.cursor = "crosshair";
+
+// variables
+const TIMEOUT = 10;
 let fillable = false;
-let PLUS_DE_COMPTEURS = 0;
 let initial = Date.now();
 
+// counters
+let counter = 0,
+    anti_counter = TIMEOUT,
+    PLUS_DE_COMPTEURS = 0,
+    roll = 0, max_roll = 0;
+
+// timers
 let timer = () => {
     setTimeout(() => {
         if (anti_counter > 0) {
@@ -14,19 +21,31 @@ let timer = () => {
             timer()
         } else {
             document.getElementById("add").innerText = "add";
-            anti_counter = timeout;
+            anti_counter = TIMEOUT;
             document.getElementById("add").classList.remove("fatigue");
             fillable = true;
         }
     }, 1000);
 }
-
 let anotherTimer = () => {
     setTimeout(() => {
         document.getElementById("formbutton").style.display = "block";
     },5000);
 }
+let rolling = () => {
+    setTimeout(() => {
+        if (roll < max_roll) {
+            roll += 5;
+            document.getElementById("formbutton").style.transform = "rotateZ(" + roll + "deg)";
+            rolling();
+        } else if(roll === 360) {
+            roll = 0;
+        }
+    }, 10);
+}
 
+// add button
+timer();
 function handle_add() {
     let out = true;
         if (fillable){
@@ -53,7 +72,7 @@ function handle_add() {
             table.appendChild(new_row);
             counter++;
             let parent = document.getElementsByTagName("tbody")[0];
-            let children = parent.children; 
+            let children = parent.children;
             console.log(children[counter-1]);
             children[counter-1].classList.add("pasDernier");
             // alert("PANDA");
@@ -85,7 +104,9 @@ function handle_add() {
         }
     }
 }
+document.getElementById("add").onclick = handle_add;
 
+// reset button
 let handle_remove = () => {
     let table = document.getElementById("listOfNameWhatALongIdForUHugo");
     // console.log(counter);
@@ -94,9 +115,11 @@ let handle_remove = () => {
         table.deleteRow(1);
     }
 }
-
-timer();
-document.getElementById("add").onclick = handle_add;
 document.getElementById("reset").onclick = handle_remove;
 
-
+// do a barrel roll
+let handle_barrel_roll = () => {
+    max_roll = (max_roll===180)?360:180;
+    rolling();
+}
+document.getElementById("barrel_roll").onclick = handle_barrel_roll;
